e55750e48ad20cec1ed5981772e8cfaf97bed927
Danylo Kolesnikov <danylokolesnikov@gmail.com>
2018-11-20 23:26:44 +0200

    Vuetify + components structure

---
+++ src/components/User/ProfileEmail.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="email">
+    Profile Email
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="css">
+</style>
